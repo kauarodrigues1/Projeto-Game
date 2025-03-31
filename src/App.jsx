1ba5fba75a4c1a46a35ad30e28c8585e109d15a9
Kaua <rodrigueskaua76@gmail.com>
2025-03-31 11:44:36 -0300

    criando as rotas no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Error from "./routes/Error"
+import Login from "./routes/Login"
+import Home from "./routes/Home"
+import Filtro from "./routes/Filtro"
+import Jogador from "./routes/Jogador"
+import Aside from "./routes/Aside"
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/filtro" element={<Filtro/>}/>
+        <Route path="/jogador" element={<Jogador/>}/>
+      </Routes>
+      <Aside/>
+      <Footer/>
+    </Router>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <h5>@2025</h5>
     </div>
   )
 }
